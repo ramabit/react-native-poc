91b9c7405b4d5404fa07a9534609fafc73635803
Fernando Ramirez <f.e.ramirez94@gmail.com>
2017-04-27 17:55:31 -0300

    uploading project

---
+++ index.ios.js
@@ -0,0 +1,20 @@
+/*
+ * index.ios.js file
+ */
+
+// import a library to help create a Component
+import React from 'react';
+import { View, AppRegistry } from 'react-native';
+import Header from './src/components/Header';
+import AlbumList from './src/components/AlbumList';
+
+// create a Component
+const App = () => (
+  <View style={{ flex: 1 }}>
+    <Header headerText={'Albums'} />
+    <AlbumList />
+  </View>
+);
+
+// render it to the device
+AppRegistry.registerComponent('albums', () => App);
---
+++ src/components/AlbumList.js
@@ -0,0 +1,31 @@
+import React, { Component } from 'react';
+import { ScrollView } from 'react-native';
+import Axios from 'axios';
+import AlbumDetail from './AlbumDetail';
+
+class AlbumList extends Component {
+  state = { albums: [] };
+
+  // load data before showing UI
+  componentWillMount() {
+    Axios.get('https://rallycoding.herokuapp.com/api/music_albums')
+      .then(response => this.setState({ albums: response.data }));
+  }
+
+  renderAlbums() {
+    return this.state.albums.map(album =>
+      <AlbumDetail key={album.title} album={album} />);
+  }
+
+  // render UI
+  render() {
+    return (
+      <ScrollView>
+        {this.renderAlbums()}
+      </ScrollView>
+    );
+  }
+
+}
+
+export default AlbumList;
